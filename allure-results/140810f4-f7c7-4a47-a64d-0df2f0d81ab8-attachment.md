7269f1e27e8f3776cf915cf780deb097c4e3f77c
Alex <alexamuek@gmail.com>
2025-07-06 20:32:53 +0800

    init

---
+++ allure-results/140810f4-f7c7-4a47-a64d-0df2f0d81ab8-attachment.md
@@ -0,0 +1,19 @@
+# Page snapshot
+
+```yaml
+- text: This is just a demo of TodoMVC for testing, not the
+- link "real TodoMVC app.":
+  - /url: https://todomvc.com/
+- heading "todos" [level=1]
+- textbox "What needs to be done?"
+- contentinfo:
+  - paragraph: Double-click to edit a todo
+  - paragraph:
+    - text: Created by
+    - link "Remo H. Jansen":
+      - /url: http://github.com/remojansen/
+  - paragraph:
+    - text: Part of
+    - link "TodoMVC":
+      - /url: http://todomvc.com
+```
---
+++ tests/example.spec.js
@@ -0,0 +1,49 @@
+// @ts-check
+/*import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});*/
+
+import { test } from '@playwright/test'
+// import { allure } from 'allure-playwright'
+import * as allure from 'allure-js-commons'
+import { ContentType } from 'allure-js-commons'
+
+
+test('Example test', async ({ page }) => {
+  await allure.step('Открытие главной страницы', async () => {
+    await page.goto('https://demo.playwright.dev/todomvc/')
+  })
+
+   // Делаем скриншот и прикрепляем его автоматически
+  // const screenshot = await page.screenshot();
+  // await allure.attachment('Скриншот страницы', screenshot, 'image/png');
+  // Текстовое вложение
+  // await allure.attachment('Текстовый файл', 'Содержимое', 'text/plain');
+  await allure.attachmentPath('Screenshot', '/home/alexamuek/hexlet/hexlet-playwright/screens/image.png', {
+    contentType: ContentType.PNG,
+    fileExtension: 'png',
+  })
+
+  // Можно даже сохранять текстовые файлы
+  await allure.attachment('Текстовый файл', 'Содержимое', ContentType.TEXT)
+  await allure.step('Проверка заголовка', async () => {
+    const title = await page.title()
+    test.expect(title).toBe('Example Domain')
+  })
+})
+
